75f8cf929fbd460dd7fa6aaaf2416cc04119009f
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-07-25 00:08:43 +0100

    Added array analysis

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
- const capitalise = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-}
+const capitalise = (string) => {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 const reverseString = (str) => {
-    return str.split('').reverse().join('')
-}
+	return str.split("").reverse().join("");
+};
 
-const calculator =  {
-    add: function(a,b) {
-        return a + b
-    },
-    subtract: function(a,b) {
-        return a - b
-    },
-    divide: function(a,b){
-        return a / b
-    },
-    multiply: function(a,b){
-        return a * b
-    }
+const calculator = {
+	add: function (a, b) {
+		return a + b;
+	},
+	subtract: function (a, b) {
+		return a - b;
+	},
+	divide: function (a, b) {
+		return a / b;
+	},
+	multiply: function (a, b) {
+		return a * b;
+	},
 };
 
 const cesarCipher = (string, shiftVal) => {
-    let output = "";
-    for (let i = 0; i < string.length; i++){
-        let char = string[i].charCodeAt();
-        if ((char >= 65 && char <= 90) || (char >= 97 && char <= 122)){
-            char += shiftVal;
-            if((char > 90 && string[i] <= 'Z' || char > 122)){
-                char -= 26
-            }
-        }
-        output += String.fromCharCode(char)
-    }
-    return output
-}
+	let output = "";
+	for (let i = 0; i < string.length; i++) {
+		let char = string[i].charCodeAt();
+		if ((char >= 65 && char <= 90) || (char >= 97 && char <= 122)) {
+			char += shiftVal;
+			if ((char > 90 && string[i] <= "Z") || char > 122) {
+				char -= 26;
+			}
+		}
+		output += String.fromCharCode(char);
+	}
+	return output;
+};
 
-console.log(cesarCipher("The Odin Project is useful", 3))
-
-module.exports = {reverseString: reverseString , capitalise: capitalise, calculator: calculator, cesarCipher: cesarCipher}
+const analyseArray = (array) => {
+	const min = Math.min.apply(null, array);
+	const max = Math.max.apply(null, array);
+	const length = array.length;
+	const average = array.reduce((curr, prev) => curr + prev, 0) / length;
+	return {
+		average: average,
+		length: length,
+		max: max,
+		min: min,
+	};
+};
+module.exports = {
+	reverseString: reverseString,
+	capitalise: capitalise,
+	calculator: calculator,
+	cesarCipher: cesarCipher,
+	analyseArray: analyseArray,
+};
--- index.test.js
+++ index.test.js
@@ -1,45 +1,53 @@
-const formulas = require('./index.js')
+const formulas = require("./index.js");
 
-
-test('first string letter to be capitalised',() => {
-    expect(formulas.capitalise("paint")).toBe("Paint")
-})
-
-test('to be reversed', () => {
-    expect(formulas.reverseString('st')).toBe('ts')
-})
-
-test('Reverse string', () => {
-    expect(formulas.reverseString('abcd')).toBe('dcba');
+test("first string letter to be capitalised", () => {
+	expect(formulas.capitalise("paint")).toBe("Paint");
 });
 
-test('add 2 numbers', () => {
-    expect(formulas.calculator.add(2,2)).toBe(4)
-})
+test("to be reversed", () => {
+	expect(formulas.reverseString("st")).toBe("ts");
+});
 
-test('subtract 2 numbers', () => {
-    expect(formulas.calculator.subtract(5,3)).toBe(2)
-})
+test("Reverse string", () => {
+	expect(formulas.reverseString("abcd")).toBe("dcba");
+});
 
-test('divide 2 numbers', () => {
-    expect(formulas.calculator.divide(12,3)).toBe(4)
-})
+test("add 2 numbers", () => {
+	expect(formulas.calculator.add(2, 2)).toBe(4);
+});
 
-test('multiply 2 numbers', () => {
-    expect(formulas.calculator.multiply(5,3)).toBe(15)
-})
+test("subtract 2 numbers", () => {
+	expect(formulas.calculator.subtract(5, 3)).toBe(2);
+});
 
-test('Cesar shift 1', () => {
-    expect(formulas.cesarCipher("HeLLo",3)).toBe("KhOOr")
-})
+test("divide 2 numbers", () => {
+	expect(formulas.calculator.divide(12, 3)).toBe(4);
+});
 
-test('Cesar shift 2', () => {
-    expect(formulas.cesarCipher("Hello, World!",3)).toBe("Khoor, Zruog!")
-})
+test("multiply 2 numbers", () => {
+	expect(formulas.calculator.multiply(5, 3)).toBe(15);
+});
 
-test('Cesar shift 3', () => {
-    expect(formulas.cesarCipher("The Odin Project is useful",3)).toBe("Wkh Rglq Surmhfw lv xvhixo")
-})
+test("Cesar shift 1", () => {
+	expect(formulas.cesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
 
+test("Cesar shift 2", () => {
+	expect(formulas.cesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
 
+test("Cesar shift 3", () => {
+	expect(formulas.cesarCipher("The Odin Project is useful", 3)).toBe(
+		"Wkh Rglq Surmhfw lv xvhixo",
+	);
+});
 
+test("Array analysis", () => {
+	let object = formulas.analyseArray([1, 6, 9, 12, 66, 555, 22, 5]);
+	expect(object).toEqual({
+		average: 84.5,
+		length: 8,
+		max: 555,
+		min: 1,
+	});
+});
